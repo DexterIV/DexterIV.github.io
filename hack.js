8ff3ef7ab07b6b80802cba3944a7364b6a12ce1c
DexterIV <wiedzmenpl@gmail.com>
2019-11-24 23:58:43 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,4 +1,6 @@
-$('#overlay').load(function(){
+setTimeout(function(){
+    $('#overlay').load(function(){
     $('#overlay').contents().find('#WIX_ADS').hide();
     $('#overlay').contents().find('#SITE_HEADER').hide();
-});
+       })
+                     }, 3000);
