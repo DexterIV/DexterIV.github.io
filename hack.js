61c8a8f47f498dff631d1c2d07faf66ff5792bc7
DexterIV <wiedzmenpl@gmail.com>
2019-11-24 23:49:09 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,5 +1,7 @@
 setTimeout(function(){ 
+ 
   var iframe = document.getElementById('overlay');
+  iframe.contentWindow.eval('function foo(){ console.log("Im in a frame",window); }');
 var doc = iframe.contentWindow.document;
   doc.getElementById('WIX_ADS').style.display='none';
   doc.getElementById('SITE_HEADER').style.margin=0;
