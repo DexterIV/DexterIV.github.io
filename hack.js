945474965ba66739e078976d255f691d29204a6a
DexterIV <wiedzmenpl@gmail.com>
2019-11-24 23:56:25 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,8 +1,4 @@
-setTimeout(function(){ 
- 
-  var iframe = document.getElementById('overlay');
-  iframe.contentWindow.eval('function foo(){ console.log("Im in a frame",window); }');
-var doc = iframe.contentWindow.document;
-  doc.getElementById('WIX_ADS').style.display='none';
-  doc.getElementById('SITE_HEADER').style.margin=0;
-}, 5000);
+$('#overlay').load(function(){
+    $('#overlay').contents().find('#WIX_ADS').hide();
+    $('#overlay').contents().find('#SITE_HEADER').hide();
+});
